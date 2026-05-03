024d9a99de618a3d53cc92382d4aa4587b932b84
y0KAM <106538211+hoanghieu2244@users.noreply.github.com>
2026-05-04 05:31:00 +0700

    fix: remove CustomCursor to fix Vercel build
    - Removed CustomCursor component causing import errors
    - Using simpler App.jsx without cursor effects
    - Will fix motion imports properly later

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,10 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import CustomCursor from './components/CustomCursor'
 
 export default function App() {
   return (
-    <div className="min-h-screen cursor-none" data-theme="dark">
-      <CustomCursor />
-
+    <div className="min-h-screen" data-theme="dark">
       <div className="relative z-10">
         <Navbar />
         <Hero />
